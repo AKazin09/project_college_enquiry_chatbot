1c36bd7699f0fd59e7f04999bebbff635038084e
AKazin09 <ankit281067@gmail.com>
2021-06-26 18:05:51 +0530

    adfasf

---
+++ src/components/background/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Canvas from 'components/canvas';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="header-info">
+            </div>
+            <Canvas />
+        </div>
+    );
+}
+
+export default App;
